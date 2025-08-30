76d26c001887d8a2f27e3c54defb57f626866b5d
karamo10 <pabimamie@gmail.com>
2025-08-30 07:35:12 +0000

    first backend commit

---
+++ server.js
@@ -0,0 +1,11 @@
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("data/products.json");
+const middlewares = jsonServer.defaults();
+
+server.use(middlewares);
+server.use(router); 
+const port = process.env.PORT || 5000;
+server.listen(port, () => {
+    console.log(`JSON server is running on port ${port}`);
+})
